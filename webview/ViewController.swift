5fe83cc4d1342da7219d5755367fe86e71844b84
pankaj kumar <pankaj@pankajs-MacBook-Pro.local>
2017-06-14 20:35:29 +0530

    Raju g

--- webview/ViewController.swift
+++ webview/ViewController.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIWebViewDelegate {
+    @IBOutlet weak var web_View: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIWebView.loadRequest(web_View)(URLRequest(url: URL(string: "https://google.com")!))
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func webViewDidStartLoad(_ webView : UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        print("AA")
+    }
+    
+    func webViewDidFinishLoad(_ webView : UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        print("BB")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+
         // Dispose of any resources that can be recreated.
     }
 
